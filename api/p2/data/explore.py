07ba90e4a11664917e9cc5cbdadbde4a6575737b
vicoduor <victoroduorr@gmail.com>
2024-05-13 18:18:03 +0300

    .

---
+++ api/p2/data/explore.py
@@ -0,0 +1,47 @@
+import sqlite3
+from model.creature import Creature
+
+DB_NAME = "cryptid.db"
+conn = sqlite3.connect(DB_NAME)
+curs = conn.cursor()
+
+
+def init():
+    curs.execute("create table creature(name, description, country, area, aka)")
+
+def row_to_model(row: tuple) -> Creature:
+    name, description, country, area, aka = row
+    return Creature(name, description, country, area, aka)
+
+def model_to_dict(creature: Creature) -> dict:
+    return creature.model_dump()
+
+def get_one(name: str) ->  Creature:
+    qry = "select * from creature where name=:name"
+    params = {"name":name}
+    curs.execute(qry, params)
+    row = curs.fetchone()
+    return row_to_model(row)
+
+def get_all(name: str) -> list[Creature]:
+    qry = "select * from creature"
+    curs.execute(qry)
+    rows = list(curs.fetchall())
+    return [row_to_model(row) for row in rows]
+
+def create(creature: Creature):
+    qry="""insert into creature values
+            (:name, :description, :country, :area, :aka)"""
+    params = model_to_dict(creature)
+    curs.execute(qry,params)
+
+def modify(creature: Creature):
+    return creature
+
+def replace(creature: Creature):
+    return creature
+
+def delete(creature: Creature):
+    qry ="delete from creature where name=: name"
+    params = {"name":creature.name}
+    curs.execute(qry, params)
